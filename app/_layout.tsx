62dfd8baa6faa62680342da082dc49534309a0b8
Michał Parkoła <michal.parkola@gmail.com>
2024-06-07 21:47:23 +0200

    Install react-query and wrap the Stack (of screens) in a QueryClientProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,31 +3,36 @@ import { Text, Pressable } from "react-native";
 import { handleLogout } from "@/helpers/supabase";
 import { HeaderButtonProps } from "@react-navigation/native-stack/lib/typescript/src/types";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 export const unstable_settings = {
   // Ensure any route can link back to `/`
   initialRouteName: "index",
 };
 
 export default function RootLayout() {
+  const queryClient = new QueryClient();
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: "lightgreen" },
-        headerRight: (props: HeaderButtonProps) => {
-          return (
-            <Pressable onPress={handleLogout}>
-              <Text style={{ marginRight: 12 }}>Logout</Text>
-            </Pressable>
-          );
-        },
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{
-          title: "Do 100 reps",
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: "lightgreen" },
+          headerRight: (props: HeaderButtonProps) => {
+            return (
+              <Pressable onPress={handleLogout}>
+                <Text style={{ marginRight: 12 }}>Logout</Text>
+              </Pressable>
+            );
+          },
         }}
-      />
-    </Stack>
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Do 100 reps",
+          }}
+        />
+      </Stack>
+    </QueryClientProvider>
   );
 }
