c0db7b364dbf5d3c83f7624ecf4f20467a430788
Michał Parkoła <michal.parkola@gmail.com>
2024-06-12 01:13:29 +0200

    Feature: Get rep notes from Supabase and display in RepView

--- components/RepView.tsx
+++ components/RepView.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import { Text, TextInput, Pressable } from "react-native";
+import { Text, TextInput, Pressable, FlatList, View } from "react-native";
 import { Calendar } from "react-native-calendars";
+import { useQuery } from "@tanstack/react-query";
+import { getNotesByRepId } from "@/helpers/supabase-queries";
 
 interface Rep {
   id: string;
@@ -27,6 +29,18 @@ export default function RepView({ rep, onChange }: RepViewProps) {
     onChange(repSummary, dateString);
   }
 
+  // get notes
+  const {
+    isPending: isPendingNotes,
+    error: errorNotes,
+    data: notes,
+  } = useQuery({
+    queryKey: ["notes", rep.id],
+    queryFn: () => getNotesByRepId(rep.id),
+  });
+
+  console.log(notes);
+
   return (
     <>
       {isEditingDate ? (
@@ -37,25 +51,50 @@ export default function RepView({ rep, onChange }: RepViewProps) {
         />
       ) : (
         <Text style={{ margin: 12 }} onPress={() => setIsEditingDate(true)}>
-          {date}
+          Created at: {date}
         </Text>
       )}
 
+      <Text style={{ margin: 12 }}>Rep summary:</Text>
       {isEditingRepSummary ? (
         <TextInput
-          style={{ margin: 12 }}
+          style={{ margin: 6, padding: 6 }}
           value={repSummary}
           onChangeText={setRepSummary}
           onSubmitEditing={() => {
             setIsEditingRepSummary(false);
             onChange(repSummary, date);
           }}
+          onBlur={() => {
+            setIsEditingRepSummary(false);
+            setRepSummary(rep.summary);
+          }}
           autoFocus={true}
         />
       ) : (
-        <Pressable onPress={() => setIsEditingRepSummary(true)}>
-          <Text style={{ margin: 12 }}>{repSummary}</Text>
-        </Pressable>
+        <>
+          <Pressable onPress={() => setIsEditingRepSummary(true)}>
+            <Text style={{ margin: 12 }}>{repSummary}</Text>
+          </Pressable>
+        </>
+      )}
+
+      {isPendingNotes || errorNotes || notes.length == 0 ? (
+        <Text style={{ margin: 12 }}>No notes (yet)</Text>
+      ) : (
+        <FlatList
+          style={{ margin: 12 }}
+          data={notes}
+          ListHeaderComponent={<Text style={{ marginBottom: 12 }}>Notes:</Text>}
+          renderItem={({ item, index }) => (
+            <Text style={{ marginTop: 12, marginBottom: 12 }}>
+              {item.created_at.substring(0, 10)} {item.text}
+            </Text>
+          )}
+          ItemSeparatorComponent={() => (
+            <View style={{ height: 1, backgroundColor: "#ccc" }} />
+          )}
+        />
       )}
     </>
   );
--- helpers/supabase-queries.ts
+++ helpers/supabase-queries.ts
@@ -1,73 +1,82 @@
 import { supabase } from "@/helpers/supabase";
 
 export async function getSupabaseUserId() {
-    const { data, error } = await supabase.auth.getUser();
-    if (error) {
-      throw new Error(error.message);
-    } else {
-      return data.user.id;
-    }
+  const { data, error } = await supabase.auth.getUser();
+  if (error) {
+    throw new Error(error.message);
+  } else {
+    return data.user.id;
   }
-  
-export async function getSupabasePractices(userid: string) {
-    const { data, error } = await supabase
-      .from("Practices")
-      .select()
-      .eq("user_id", userid)
-      .not("do100reps_title", "is", null);
-  
-    if (error) {
-      throw new Error(error.message);
-    } else {
-      return data;
-    }
-  }
+}
 
-  export async function getSupabasePracticeById(practiceid: string) {
-    let userid = await getSupabaseUserId();
-    const { data, error } = await supabase
+export async function getSupabasePractices(userid: string) {
+  const { data, error } = await supabase
+    .from("Practices")
+    .select()
+    .eq("user_id", userid)
+    .not("do100reps_title", "is", null);
+
+  if (error) {
+    throw new Error(error.message);
+  } else {
+    return data;
+  }
+}
+
+export async function getSupabasePracticeById(practiceid: string) {
+  let userid = await getSupabaseUserId();
+  const { data, error } = await supabase
     .from("Practices")
     .select()
     .eq("user_id", userid)
     .eq("id", practiceid);
-  
-    if (error) {
-      throw new Error(error.message);
-    } else {
-      return data[0];
-    }
+
+  if (error) {
+    throw new Error(error.message);
+  } else {
+    return data[0];
   }
+}
 
-  export async function getSupabaseRepsByPracticeName(practiceName: string) {
-    const { data, error } = await supabase
-      .from("Reps")
-      .select()
-      .eq("practice", practiceName)
-      .order("created_at", { ascending: false })
-      .order("summary", { ascending: false });
-  
-    if (error) {
-      throw new Error(error.message);
-    } else {
-      return data;
-    }
+export async function getSupabaseRepsByPracticeName(practiceName: string) {
+  const { data, error } = await supabase
+    .from("Reps")
+    .select()
+    .eq("practice", practiceName)
+    .order("created_at", { ascending: false })
+    .order("summary", { ascending: false });
+
+  if (error) {
+    throw new Error(error.message);
+  } else {
+    return data;
   }
+}
 
-  export async function getSupabaseRepsByPracticeId(practiceId: string) {
+export async function getSupabaseRepsByPracticeId(practiceId: string) {
+  let practice = await getSupabasePracticeById(practiceId);
+  const practiceName = practice ? practice[0].name : "";
 
-    let practice = await getSupabasePracticeById(practiceId);
-    const practiceName = practice ? practice[0].name : "";
+  const { data, error } = await supabase
+    .from("Reps")
+    .select()
+    .eq("practice", practiceName)
+    .order("created_at", { ascending: false })
+    .order("summary", { ascending: false });
 
-    const { data, error } = await supabase
-      .from("Reps")
-      .select()
-      .eq("practice", practiceName)
-      .order("created_at", { ascending: false })
-      .order("summary", { ascending: false });
-  
-    if (error) {
-      throw new Error(error.message);
-    } else {
-      return data;
-    }
+  if (error) {
+    throw new Error(error.message);
+  } else {
+    return data;
   }
+}
+
+export async function getNotesByRepId(rep_id: string) {
+  const { data, error } = await supabase.from("RepNotes").select();
+
+  if (error) {
+    throw new Error(error.message);
+  } else {
+    return data;
+  }
+}
