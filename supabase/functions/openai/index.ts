4c1412ce9df88ef58934ed7b6aa1e18b388d1bc1
Michał Parkoła <michal.parkola@gmail.com>
2024-06-12 15:42:27 +0200

    Supabase Edge Functions: OpenAI example

--- supabase/functions/openai/index.ts
+++ supabase/functions/openai/index.ts
@@ -1,32 +1,23 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
-// Setup type definitions for built-in Supabase Runtime APIs
-/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
-
-console.log("Hello from Functions!")
+import OpenAI from "https://deno.land/x/openai@v4.24.0/mod.ts";
 
 Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
+  const { query } = await req.json();
+  const apiKey = Deno.env.get("OPENAI_API_KEY");
+  const openai = new OpenAI({
+    apiKey: apiKey,
+  });
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  // Documentation here: https://github.com/openai/openai-node
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: query }],
+    // Choose model from here: https://platform.openai.com/docs/models
+    model: "gpt-3.5-turbo",
+    stream: false,
+  });
 
-/* To invoke locally:
+  const reply = chatCompletion.choices[0].message.content;
 
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/openai' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
+  return new Response(reply, {
+    headers: { "Content-Type": "text/plain" },
+  });
+});
